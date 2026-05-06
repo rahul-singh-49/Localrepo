980230b0266091bb82b6728c0775f339f5d33125
rahul-singh-49 <rahularathor222@gmail.com>
2026-05-06 23:10:25 +0530

    update3

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.70.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+}
+
+resource "azurerm_resource_group" "rg1-new" {
+  name     = "RG"
+  location = "West Europe"
+}
+resource "azurerm_storage_account" "st1" {
+  name                     = "st1kasyorage"
+  resource_group_name      = azurerm_resource_group.rg1-new.name
+  location                 = azurerm_resource_group.rg1-new.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+}
